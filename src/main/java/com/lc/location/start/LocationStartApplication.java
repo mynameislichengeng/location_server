7e73c28927b579664349c568e092073befc2e2a1
李成 <licheng01@mucang.cn>
2024-08-16 09:55:59 +0800

    feat:init

---
+++ src/main/java/com/lc/location/start/LocationStartApplication.java
@@ -0,0 +1,13 @@
+package com.lc.location.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationStartApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lc/location/start/controller/LocationController.java
@@ -0,0 +1,20 @@
+package com.lc.location.start.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by licheng01
+ * @date 2024/8/16 9:51
+ * @description
+ */
+@RestController
+public class LocationController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Location Server!";
+    }
+
+}
